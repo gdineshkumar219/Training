1fdacb224be64505f5970f41727c3d6da3485668
Dinesh kumar <dineshkumar.g@metamation.com>
2023-07-24 17:19:22 +0530

    Assignment 02

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
-}
+﻿using System;
+Console.WriteLine ("I'm thinking of a number between 1 and 100.");
+Console.WriteLine ("You have to guess it.");
+int num, targetNumber = new Random().Next (1, 101);
+for (; ; ) {
+    Console.Write ("Enter your guess:");
+    num = int.Parse (Console.ReadLine ());
+    if (num == targetNumber) {
+        Console.WriteLine ("Your guess is correct"); break;
+    } 
+    else if (num > targetNumber)
+        Console.WriteLine ("Your guess is too high");
+    else
+        Console.WriteLine ("Your guess is too small");
+ }
