3dd8565fb3d8670c49af3fb5ea29192bc968027e
Dinesh kumar <dineshkumar.g@metamation.com>
2023-07-26 16:26:19 +0530

    Do while is removed in A02

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine ("I'm thinking of a number between 1 and 100.");
 Console.WriteLine ("You have to guess it.");
 int targetNumber = new Random ().Next (1, 101);
-for (int i=1; ; i++) {
+for (int i = 1; ; i++) {
    Console.Write ("Enter your guess:");
    string value = Console.ReadLine ();
-   if (!int.TryParse (value, out int result) || (result < 1 || result > 100)) { Console.WriteLine ("Please enter a value between 1 and 100");  }
+   if (!int.TryParse (value, out int result) || (result < 1 || result > 100)) { Console.WriteLine ("Please enter a value between 1 and 100"); }
    if (result == targetNumber) {
       Console.WriteLine ($"Your guess is correct at {i} tries.");
       Console.WriteLine (result > targetNumber ? "Your guess is too high" : "Your guess is too small");
@@ -15,7 +15,6 @@ for (int i=1; ; i++) {
 bool PlayAgain () {
    Console.WriteLine ("Do you want to play again? (Y)es / (N)o");
    char key = Console.ReadKey (true).KeyChar;
-   //char response = Console.ReadKey (true).KeyChar;
    return key == 'y';
 }
 
