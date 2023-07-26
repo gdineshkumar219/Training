b5d9b24f6794c21b76769a9a9f6b4dd5641811fe
Dinesh kumar <dineshkumar.g@metamation.com>
2023-07-26 09:46:54 +0530

    Changed all corrections in A02

--- Program.cs
+++ Program.cs
@@ -3,16 +3,16 @@
 Console.WriteLine ("I'm thinking of a number between 1 and 100.");
 Console.WriteLine ("You have to guess it.");
 int targetNumber = new Random ().Next (1, 101);
-for (; ; ) {
-    Console.Write ("Enter your guess:");
-    string value=Console.ReadLine ();
-   if (int.TryParse (value, out int result)) return result;
-   if (num == targetNumber) {
-        Console.WriteLine ("Your guess is correct"); break;
-    } 
-    else if (num > targetNumber)
-        Console.WriteLine ("Your guess is too high");
-    else
-        Console.WriteLine ("Your guess is too small");
- }
+for (int i = 1; ; i++) {
+   Console.Write ("Enter your guess:");
+   string value = Console.ReadLine ();
+   if (!int.TryParse (value, out int result)) { Console.WriteLine ("Please enter a value between 1 and 100"); continue; }
+   if (result == targetNumber) {
+      Console.WriteLine ($"Your guess is correct at {i} tries"); break;
+   } else if (result > targetNumber)
+      Console.WriteLine ("Your guess is too high");
+   else
+      Console.WriteLine ("Your guess is too small");
+}
+
 
