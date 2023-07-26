438d41113bebf688ef875061b12977cfe39879bb
Dinesh kumar <dineshkumar.g@metamation.com>
2023-07-26 17:14:28 +0530

    Removed the wrong statement after if condition in A02

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ for (int i = 1; ; i++) {
    if (!int.TryParse (value, out int result) || (result < 1 || result > 100)) { Console.WriteLine ("Please enter a value between 1 and 100"); }
    if (result == targetNumber) {
       Console.WriteLine ($"Your guess is correct at {i} tries.");
-      Console.WriteLine (result > targetNumber ? "Your guess is too high" : "Your guess is too small");
       if (!PlayAgain ()) break;
       else { targetNumber = new Random ().Next (1, 101); i = 0; }
    } else Console.WriteLine (result > targetNumber ? "Your guess is too high" : "Your guess is too small");
