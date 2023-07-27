fc55b15b806e3d5a5464b2860ed3259a1cfa3bfa
Dinesh kumar <dineshkumar.g@metamation.com>
2023-07-27 12:37:42 +0530

    Inverse of guessing game

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿Console.WriteLine ("Welcome to the Number Prediction Game!");
+Console.WriteLine ("Think of a number between 0 and 127, and I will try to guess it in 7 questions.");
+int guess = 0;
+int lowerBound = 0;
+int upperBound = 127;
+int bitValue = (lowerBound + upperBound + 1) / 2;
+bool correctGuess = false;
+for (int questionNumber = 1; questionNumber <= 7; questionNumber++) {
+   Console.WriteLine ($"Question {questionNumber}: Is the number greater than {guess + bitValue}? ((y)es/ (n)o)");
+   char key = Console.ReadKey (true).KeyChar;
+   if (key == 'y') {
+      guess += bitValue;
+      lowerBound += bitValue;
+   } else if (key == 'n') {
+      upperBound -= bitValue;
+   } else {
+      Console.WriteLine ("Invalid input. Please answer 'yes' or 'no'.");
+      //Console.WriteLine ($"Question {questionNumber}: Is the number greater than {guess + bitValue}? (yes/no)");
+      questionNumber--;
+      bitValue = bitValue * 2;
+   }
+   bitValue /= 2;
+   if (lowerBound == upperBound) {
+      correctGuess = true;
+      break;
+   }
+}
+if (correctGuess) {
+   Console.WriteLine ($"I guessed it! The number you were thinking of is {guess + 1}.");
+} else {
+   Console.WriteLine ("I could not guess the number correctly in 7 questions. You win!");
 }
