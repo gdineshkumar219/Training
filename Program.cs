26bf9345f76aeda572c501713768a0330f607ca1
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-02 16:53:22 +0530

    Made some changes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿char[] letters = { 'U', 'X', 'A', 'L', 'T', 'N', 'E' }; // Define the seed letters for the Spelling Bee puzzle
 string[] wordList = File.ReadAllLines (@"C:\New folder\words.txt");// Read the word list from a file into an array 
-List<(string word, int score)>validWords = new List<(string word, int score)>();//List to store the valid words along with score
 int total = 0;//Variable to score the total score of the valid words
+string[] validWords = new string[wordList.Length]; int validWordIndex = 0;
+int[] scores = new int[wordList.Length];
 foreach (var word in wordList) {
    // Check if the word meets the Spelling Bee rules: at least 4 letters long, contains 'U', and only uses the seed letters
    if (word.Length >= 4 && word.Contains ('U') && (word.All (letters.Contains))) {
@@ -10,23 +11,25 @@ foreach (var word in wordList) {
          score = word.Length + 7; // Bonus points for pangram
       } else {
          score = word.Length == 4 ? 1 : word.Length;
-         Console.ResetColor ();
       }
       total += score;
-      validWords.Add((word, score));
+      validWords[validWordIndex] = word;
+      scores[validWordIndex] = score;
+      validWordIndex++;
+      //validWords.Add((word, score));
    }
 }
-Console.ResetColor();
-validWords.Sort ((a, b) => b.score.CompareTo (a.score));
-foreach(var (word,score) in validWords) {
-   if (score == 15) 
-      Console.ForegroundColor = ConsoleColor.Green;Console.WriteLine ($"{score,4}. {word}");Console.ResetColor ();
+// Sort the arrays of valid words and scores in descending order based on scores
+Array.Sort (scores, validWords, 0, validWordIndex, Comparer<int>.Create ((a, b) => b.CompareTo (a)));
+// Print the formatted output
+for (int i = 0; i < validWordIndex; i++) {
+   if (scores[i] == 15) { Console.ForegroundColor = ConsoleColor.Green; }
+
+   Console.WriteLine ($"{scores[i],4}. {validWords[i]}");
+   Console.ResetColor ();
 }
 Console.WriteLine ("----");
-Console.WriteLine ($" {total} Total Score");
+// Print the total score of valid words
 
-
-
-
-
+Console.WriteLine ($"{total} Total Score");
 
