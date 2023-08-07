a934a288104de298bf65836f3a68fc40f1f57be1
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-07 14:13:04 +0530

    Modified some changes in code

--- Program.cs
+++ Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
-
-Console.Write ("Enter the number to convert:");
+﻿Console.Write ("Enter the number to convert:");
 int num = Convert.ToInt32 (Console.ReadLine ());
 string result = "";
 int rem;
-Console.WriteLine ($"The binary value of {num} is : {dec2bin (num)}");
-Console.WriteLine ($"The hexadecimal value of {num} is : {dec2hex (num)}");
+Console.WriteLine ($"The binary value of {num} is : {dec2bin (num)}.");
+Console.WriteLine ($"The hexadecimal value of {num} is : {dec2hex (num)}.");
 string dec2bin (int num) {
    while (num > 1) {
       rem = num % 2;
-      result = Convert.ToString (rem) + result;
-      num /= 2;
-
+      result = Convert.ToString (rem) + result;num /= 2;
    }
    return result;
 }
