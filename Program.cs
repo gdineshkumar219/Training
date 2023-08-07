4c8f63c16c2d7570b57064d9ad70c9c2cfdcc2c0
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-07 12:53:29 +0530

    Code for Decimal to different forms converter has been executed.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+
+Console.Write ("Enter the number to convert:");
+int num = Convert.ToInt32 (Console.ReadLine ());
+string result = "";
+int rem;
+Console.WriteLine ($"The binary value of {num} is : {dec2bin (num)}");
+Console.WriteLine ($"The hexadecimal value of {num} is : {dec2hex (num)}");
+string dec2bin (int num) {
+   while (num > 1) {
+      rem = num % 2;
+      result = Convert.ToString (rem) + result;
+      num /= 2;
+
+   }
+   return result;
 }
+string dec2hex (int num) {
+   string result = num.ToString ("X");
+   return result;
+}
+
