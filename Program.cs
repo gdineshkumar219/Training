aa22b145581c4598dbed11433dd5089b62511f37
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-08 17:04:14 +0530

    T06 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿Console.Write ("Enter the string: ");
+string userInput = Console.ReadLine ();
+string cleanedInput = RemoveSpacesAndLowercase (userInput);
+string res = IsPalindrome (cleanedInput) ? $"{userInput} is a palindrome." : $"{userInput} is not a palindrome.";
+Console.WriteLine (res);
+string RemoveSpacesAndLowercase (string input) {
+   return new string (input.ToLower ().Where (char.IsLetter).ToArray ());
+}
+bool IsPalindrome (string input) {
+   return input == new string (input.Reverse ().ToArray ());
 }
