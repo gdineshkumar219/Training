e641480dc3c4e7fc9c1375c667cbaf441eb5c682
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-08 08:48:12 +0530

    T03 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿int num;
+while (true) {
+   Console.Write ("Enter the number of terms: ");
+   if (int.TryParse (Console.ReadLine (), out num) && num >= 0) {
+      break;
+   } else {
+      Console.WriteLine ("Invalid input. Please enter a non-negative integer.");
+   }
+}
+Console.WriteLine ("Fibonacci series is:");
+for (int i = 0; i < num; i++) {
+   Console.WriteLine ($"{FibonacciSeries (i)}");
+}
+int FibonacciSeries (int num) {
+   return num <= 1 ? num : FibonacciSeries (num - 1) + FibonacciSeries (num - 2);
 }
