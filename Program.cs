dbef901e196d40969fa0512e3777309e8c987779
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-09 08:35:36 +0530

    made some changes

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
-Console.Write ("Enter the number to convert:");
-int num = Convert.ToInt32 (Console.ReadLine ());
-string result = "";
-int rem;
-Console.WriteLine ($"The binary value of {num} is : {dec2bin (num)}.");
-Console.WriteLine ($"The hexadecimal value of {num} is : {dec2hex (num)}.");
-string dec2bin (int num) {
-   while (num > 1) {
-      rem = num % 2;
-      result = Convert.ToString (rem) + result;num /= 2;
-   }  return result;
+int decimalNumber = GetIntegerInput ("Enter a decimal number:");
+string binary = Convert.ToString (decimalNumber, 2);
+string hexadecimal = decimalNumber.ToString ("X");
+Console.WriteLine ($"Binary value of {decimalNumber}: {binary}");
+Console.WriteLine ($"Hexadecimal value of {decimalNumber}: {hexadecimal}");
+int GetIntegerInput (string prompt) {
+   while (true) {
+      Console.Write (prompt);
+      if (int.TryParse (Console.ReadLine (), out int value)) return value;
+      Console.WriteLine ("Invalid input. Please enter an integer.");
+   }
 }
-string dec2hex (int num) {
-   string result = num.ToString ("X");
-   return result;
-}
-
