902c10e074e416d7bb94efc9895a496fc3a27c3c
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-09 08:11:16 +0530

    Updated the corrections

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
 ﻿Console.WriteLine ("Welcome to the Number Prediction Game!");
 Console.WriteLine ("Think of a number between 0 and 127, and I will try to guess it in 7 questions.");
-int N = 2, rem = 0, pow = 1;
-int result = 0;
+int rem = 0;
 for (int i = 1; i <= 7; i++) {
-   Console.WriteLine ($"Question {i}: Is the remainder {rem} when divided by{N}? ((y)es/ (n)o)");
-   char key = Console.ReadKey (true).KeyChar;
-   if (key == 'y') {
-      result += (int)(0 * Math.Pow (2, pow));
-   } else {
-      rem = rem + N / 2;
-      result += (int)(1 * Math.Pow (2, pow));
-   }
-   pow++; N = (int)(Math.Pow (2, i + 1));
+   Console.WriteLine ($"Question {i}: Is the remainder {rem} when divided by {Math.Pow (2, i)}? ((y)es / (n)o)");
+   rem += (Console.ReadKey (true).Key == ConsoleKey.N) ? (int)Math.Pow (2, (i - 1)) : 0;
 }
 Console.WriteLine ($"I guessed it! The number you were thinking of is {rem}.");
+
+
