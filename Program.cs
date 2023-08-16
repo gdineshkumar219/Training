f214605a6296739c9dcfdb2b798e8b19c733b24e
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-16 14:28:59 +0530

    T07 completed

--- Program.cs
+++ Program.cs
@@ -1,97 +1,83 @@
-﻿//using System.Text;
-//int boardSize = 8;
-//char[,] board = new char[boardSize, boardSize];
-//Console.OutputEncoding = Encoding.UTF8;
-//for (int row = 0; row < boardSize; row++) {
-//   for (int col = 0; col < boardSize; col++) {
-//      bool isWhiteSquare = (row + col) % 2 == 0;
-//      char piece = ' ';
-//      if (isWhiteSquare) {
-//         piece = '\u25A1'; // White square
-//      } else {
-//         piece = '\u25A1'; // Black square
-//      }
-//      board[row, col] = piece;
-//   }
-//}
-//// Set up white pieces
-//board[0, 0] = '\u265C'; // Rook
-//board[0, 1] = '\u265E'; // Knight
-//board[0, 2] = '\u265D'; // Bishop
-//board[0, 3] = '\u265B'; // Queen
-//board[0, 4] = '\u265A'; // King
-//board[0, 5] = '\u265D'; // Bishop
-//board[0, 6] = '\u265E'; // Knight
-//board[0, 7] = '\u265C'; // Rook
-//// Set up black pieces
-//for (int col = 0; col < boardSize; col++) {
-//   board[1, col] = '\u265F'; // Pawn
-//   board[6, col] = '\u2659'; // Pawn
-//}
-//board[7, 0] = '\u2656'; // Rook
-//board[7, 1] = '\u2658'; // Knight
-//board[7, 2] = '\u2657'; // Bishop
-//board[7, 3] = '\u2655'; // Queen
-//board[7, 4] = '\u2654'; // King
-//board[7, 5] = '\u2657'; // Bishop
-//board[7, 6] = '\u2658'; // Knight
-//board[7, 7] = '\u2656'; // Rook
-//// Display the board
-//for (int row = 0; row < boardSize; row++) {
-//   for (int col = 0; col < boardSize; col++) {
-//      Console.Write (board[row, col]);
-//   }
-//   Console.WriteLine ();
-//}
-using System;
-
-namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.OutputEncoding = System.Text.Encoding.Unicode;
-
+﻿using System.Text;
+System.Console.OutputEncoding = new UnicodeEncoding ();
          for (int i = 1; i <= 8; i++) {
-            PrintLine (i);
+            PrintGrid (i);
             Console.WriteLine ();
             for (int j = 1; j <= 8; j++) {
-               Console.Write ('\u2502');
-               DisplayChessItem (i, j);
+               if (j != 9) Console.Write ('\u2502');
+               DisplayChessCoins (i, j);
             }
             Console.Write ('\u2502' + "\n");
             if (i != 8) Console.Write ('\u251C');
          }
-         PrintLine (9);
-      }
-
-      static void PrintLine (int row) {
-         char start = (row == 1) ? '\u250C' : (row == 9) ? '\u2514' : '\u251C';
-         char mid = (row == 1 || row == 9) ? '\u2500' : '\u2500';
-
-         Console.Write (start);
+         PrintGrid (9);
+    void PrintGrid (int row) {
+         if (row == 1) Console.Write ('\u250C');
+         if (row == 9) Console.Write ('\u2514');
          for (int i = 1; i <= 8; i++) {
-            Console.Write (mid + mid);
-            Console.Write ((i == 8) ? ((row == 1) ? '\u2510' : '\u2518') : mid);
+            switch (row) {
+               case 1:
+                  Console.Write ('\u2500'); Console.Write ('\u2500');
+                  Console.Write (i == 8 ? '\u2510' : '\u252C'); break;
+               case 9:
+                  Console.Write ('\u2500'); Console.Write ('\u2500');
+                  Console.Write (i == 8 ? '\u2518' : '\u2534'); break;
+               default:
+                  Console.Write ('\u2500'); Console.Write ('\u2500');
+                  Console.Write (i == 8 ? '\u2524' : '\u253C'); break;
+            }
          }
       }
-
-      static void DisplayChessItem (int i, int j) {
-         char chessPiece = ' ';
-         if ((i == 1 || i == 8) && (j == 1 || j == 8))
-            chessPiece = (i == 1) ? '\u2656' : '\u265C';
-         else if ((i == 1 || i == 8) && (j == 2 || j == 7))
-            chessPiece = (i == 1) ? '\u2658' : '\u265E';
-         else if ((i == 1 || i == 8) && (j == 3 || j == 6))
-            chessPiece = (i == 1) ? '\u2657' : '\u265D';
-         else if (i == 1 && j == 4)
-            chessPiece = '\u2655';
-         else if (i == 1 && j == 5)
-            chessPiece = '\u2654';
-         else if (i == 2)
-            chessPiece = '\u2659';
-         else if (i == 7)
-            chessPiece = '\u265F';
-
-         Console.Write (" " + chessPiece);
-      }
+      void DisplayChessCoins (int i, int j) {
+         switch (i) {
+      case 1:
+         Console.Write (" " + (j switch {
+            1 or 8 => " " + '\u2656',
+            3 or 6 => " " + '\u2657',
+            2 or 7 => " " + '\u2658',
+            4 => " " + '\u2655',
+            5 => " " + '\u2654',
+            _ => ' '
+         }));
+         break;
+      case 2:
+         Console.Write (" " + '\u2659');
+         break;
+      case 7:
+         Console.Write (" " + '\u265F');
+         break;
+      case 8:
+         Console.Write (" " + (j switch {
+            1 or 8 => " "+'\u265C',
+            3 or 6 => " " + '\u265D',
+            2 or 7 => " " + '\u265E',
+            4 =>" "+ '\u265B',
+            5 => " " + '\u265A',
+            _ => ' '
+         }));
+         break;
+      default:
+         Console.Write (" ");
+         break;
    }
+
+   //case 1:
+   //   if (j is 1 or 8) Console.Write (" " + '\u2656');
+   //   else if (j is 3 or 6) Console.Write (" " + '\u2657');
+   //   else if (j is 2 or 7) Console.Write (" " + '\u2658');
+   //   else if (j == 4) Console.Write (" " + '\u2655');
+   //   else if (j == 5) Console.Write (" " + '\u2654');
+   //   break;
+   //case 2: Console.Write (" " + '\u2659'); break;
+   //case 7: Console.Write (" " + '\u265F'); break;
+   //case 8:
+   //   if (j is 1 or 8) Console.Write (" " + '\u265C');
+   //   else if (j is 3 or 6) Console.Write (" " + '\u265D');
+   //   else if (j is 2 or 7) Console.Write (" " + '\u265E');
+   //   else if (j == 4) Console.Write (" " + '\u265B');
+   //   else if (j == 5) Console.Write (" " + '\u265A');
+   //   break;
+   //default: Console.Write (" " + " "); break;
 }
+      
+  
