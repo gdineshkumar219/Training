0346e4d0cab5c7447f116896d94020cf3897ab35
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-16 16:57:03 +0530

    modified program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,7 @@
-﻿int number = GetIntegerInput ("Enter the number to find the digital root:");
-Console.WriteLine ($"The digital root value of {number} is {CalculateDigitalRoot (number)}");
-int GetIntegerInput (string prompt) {
-   while (true) {
-      Console.Write (prompt);
-      if (int.TryParse (Console.ReadLine (), out int value) && value > 0) {
-         return value;
+﻿namespace Training {
+   internal class Program {
+      static void Main (string[] args) {
+         Console.WriteLine ("Hello, World!");
       }
-      Console.WriteLine ("Invalid input. Please enter a positive integer.");
    }
-}
-int CalculateDigitalRoot (int n) {
-   while (n > 9) {
-      int sum = 0;
-      while (n > 0) {
-         sum += n % 10;
-         n /= 10;
-      }
-      n = sum;
-   }
-   return n;
 }
