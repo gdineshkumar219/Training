c8e62f6bfca25ab518280259d9cd47e5a4a26dcc
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-16 14:57:10 +0530

    T08 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿int number = GetIntegerInput ("Enter the multiplication table you want:");
+for (int i = 1; i <= 10; i++) {
+   Console.Write ($"{number} * {i,2} = {number * i}\n");
+}
+int GetIntegerInput (string prompt) {
+   while (true) {
+      Console.Write (prompt);
+      if (int.TryParse (Console.ReadLine (), out int value)) return value;
+      Console.WriteLine ("Invalid input. Please enter an integer.");
+   }
 }
