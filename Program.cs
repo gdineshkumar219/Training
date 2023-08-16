5ad1e44117d8509155d10825680a915557a0a964
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-16 16:22:22 +0530

    T10 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿int number = GetIntegerInput ("Enter the number to check whether it is palindrome or not:");
+int originalNumber = number;
+int reversedNumber = 0;
+while (number != 0) {
+   int digit = number % 10;
+   reversedNumber = reversedNumber * 10 + digit;
+   number /= 10;
+}
+Console.WriteLine ($"The reverse of {originalNumber} is {reversedNumber}.");
+var res = originalNumber == reversedNumber ? $"{originalNumber} is a palindrome." : $"{originalNumber} is not a palindrome.";
+Console.WriteLine (res);
+int GetIntegerInput (string prompt) {
+   while (true) {
+      Console.Write (prompt);
+      if (int.TryParse (Console.ReadLine (), out int value) && value > 0) {
+         return value;
+      }
+      Console.WriteLine ("Invalid input. Please enter a positive integer.");
+   }
 }
