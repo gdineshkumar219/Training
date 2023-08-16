e79bdc13b30080e35abab2bf58c0cc7cf7892a20
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-16 08:13:32 +0530

    T02 completed

--- Program.cs
+++ Program.cs
@@ -1,46 +1,33 @@
-﻿Console.Write ("Enter the Number: ");
-var number = int.Parse (Console.ReadLine ());
+﻿long number = GetValidNumberInput ();
 Console.WriteLine ($"\nWords representation: {NumberToWords (number)}");
-Console.WriteLine ($"\nRoman numeral representation: {NumberToRoman (number)}");   
-string NumberToWords (long number) {
-   string words = "";
-   if (number < 0) return ($"Minus {NumberToWords (Math.Abs (number))}");
-   if ((number / 10000000) > 0) {
-      words += NumberToWords (number / 10000000) + " crores ";
-      number %= 10000000;
-   }
-   if ((number / 100000) > 0) {
-      words += NumberToWords (number / 100000) + " lakhs ";
-      number %= 100000;
-   }
-   if ((number / 1000) > 0) {
-      words += NumberToWords (number / 1000) + " thousand ";
-      number %= 1000;
-   }
-   if ((number / 100) > 0) {
-      words += NumberToWords (number / 100) + " hundred ";
-      number %= 100;
-   }
-   if (number > 0) {
-      if (words != "")
-         words += "and ";
-      var units = new[] { "Zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-      var tens = new[] { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-      if (number < 20)
-         words += units[number];
-      else {
-         words += tens[number / 10 - 2];
-         if (number % 10 > 0)
-            words += " " + units[number % 10];
+Console.WriteLine ($"\nRoman numeral representation: {NumberToRoman (number)}");
+long GetValidNumberInput () {
+   long number = 0;
+   while (true) {
+      Console.Write ("Enter the Number between 1 and 1000: ");
+      if (long.TryParse (Console.ReadLine (), out number) && number >= 1 && number <= 1000) {
+         return number;
       }
+      Console.WriteLine ("Please enter a valid number between 1 and 1000.");
    }
-   return words;
 }
-string NumberToRoman (int number) {
+string NumberToWords (long number) {
+   if (number == 0) return "Zero";
+   if (number < 0) return $"Minus {NumberToWords (Math.Abs (number))}";
+   string[] units = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+   string[] teens = { "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+   string[] tens = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+   if (number < 10) return units[number];
+   if (number < 20) return teens[number - 11];
+   if (number < 100) return tens[number / 10] + (number % 10 > 0 ? " " + units[number % 10] : "");
+   if (number < 1000) return $"{units[number / 100]} Hundred" + (number % 100 > 0 ? " and " + NumberToWords (number % 100) : "");
+   return $"{NumberToWords (number / 1000)} Thousand" + (number % 1000 > 0 ? " " + NumberToWords (number % 1000) : "");
+}
+string NumberToRoman (long number) {
    if (number == 0) return "No symbol";
-   if (number < 0) return ($"Minus {NumberToRoman (Math.Abs (number))}");
-   var symbols = new[] { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M", "MV'", "V'", "MX'", "X'" };
-   var val = new[] { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000, 4000, 5000, 9000, 10000 };
+   if (number < 0) return $"Minus {NumberToRoman (Math.Abs (number))}";
+   string[] symbols = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+   int[] val = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
    string roman = "";
    for (int index = val.Length - 1; index >= 0; index--) {
       while (number >= val[index]) {
