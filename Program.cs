3d92bd011da4cd2e4a07972730e9e20a899cd090
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-16 15:58:01 +0530

    T09 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿int rows = GetIntegerInput ("Enter the number of rows:") + 1;
+for (int k = 1; k <= rows; k++) {
+   PrintPatternLine (rows - k, 2 * k - 1);
+}
+for (int k = 1; k <= rows - 1; k++) {
+   PrintPatternLine (k, 2 * (rows - k) - 1);
+}void PrintPatternLine (int spaces, int stars) {
+   for (int i = 0; i < spaces; i++)
+      Console.Write (" ");
+   for (int i = 0; i < stars; i++)
+      Console.Write ("*");
+   Console.WriteLine ();
+}
+int GetIntegerInput (string prompt) {
+   int value;
+   do {
+      Console.Write (prompt);
+   } while (!int.TryParse (Console.ReadLine (), out value));
+   return value;
 }
