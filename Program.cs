fb9dea783a3fe48f66971148948077124a96a962
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-17 10:06:35 +0530

    T12 completed

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@ int val = 1, blank, i, j;
 Console.WriteLine ("Pascal's triangle");
 for (i = 0; i < rows; i++) {
    for (blank = 1; blank <= rows - i; blank++)
-      Console.Write (" ");
+      Console.Write ("  ");
    for (j = 0; j <= i; j++) {
       if (j == 0 || i == 0)
          val = 1;
       else
          val = val * (i - j + 1) / j;
-      Console.Write ($"{val}  ");
+      Console.Write ($"{val,6}");
    }
    Console.WriteLine ();
 }
