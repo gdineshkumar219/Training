51a3e090a63e1e99d85a7f091d9e568a1f8f605f
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-17 08:40:44 +0530

    modified

--- Program.cs
+++ Program.cs
@@ -1,24 +1,7 @@
-﻿int rows = GetIntegerInput ("Enter the number of rows:");
-int val = 1, blank, i, j;
-Console.WriteLine ("Pascal's triangle");
-for (i = 0; i < rows; i++) {
-   for (blank = 1; blank <= rows - i; blank++)
-      Console.Write (" ");
-   for (j = 0; j <= i; j++) {
-      if (j == 0 || i == 0)
-         val = 1;
-      else
-         val = val * (i - j + 1) / j;
-      Console.Write (val + " ");
-   }
-   Console.WriteLine ();
-}
-int GetIntegerInput (string prompt) {
-   while (true) {
-      Console.Write (prompt);
-      if (int.TryParse (Console.ReadLine (), out int value) && value > 0) {
-         return value;
+﻿namespace Training {
+   internal class Program {
+      static void Main (string[] args) {
+         Console.WriteLine ("Hello, World!");
       }
-      Console.WriteLine ("Invalid input. Please enter a positive integer.");
    }
 }
