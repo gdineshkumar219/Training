b84882da5b6ee4b0120d95359e92e01743184518
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-21 16:39:00 +0530

    Used inline method in T03

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
 ﻿int num;
 while (true) {
    Console.Write ("Enter the number of terms: ");
-   if (int.TryParse (Console.ReadLine (), out num) && num >= 0) {
-      break;
-   } else {
-      Console.WriteLine ("Invalid input. Please enter a non-negative integer.");
-   }
+   if (int.TryParse (Console.ReadLine (), out num) && num >= 0) break;
+   else Console.WriteLine ("Invalid input. Please enter a non-negative integer.");
 }
 Console.WriteLine ("Fibonacci series is:");
-for (int i = 0; i < num; i++) {
-   Console.WriteLine ($"{FibonacciSeries (i)}");
-}
-int FibonacciSeries (int num) {
-   return num <= 1 ? num : FibonacciSeries (num - 1) + FibonacciSeries (num - 2);
-}
+for (int i = 0; i < num; i++) Console.WriteLine ($"{FibonacciNumber (i)}");
+
+int FibonacciNumber (int num) => num <= 1 ? num : FibonacciNumber (num - 1) + FibonacciNumber (num - 2);
