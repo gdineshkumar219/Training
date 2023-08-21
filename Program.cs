6f7dddc9be8bd13f6959a3a5e5bb98893050d7fc
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-21 15:26:54 +0530

    A04 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+﻿string[] wordList = File.ReadAllLines (@"C:\Users\ganesamoorthydi\Downloads\words.txt");
+Dictionary<char, int> charCount = new Dictionary<char, int> ();
+foreach (string word in wordList) {
+   foreach (char ch in word) {
+      if (char.IsLetterOrDigit (ch)) {
+         var c= charCount.ContainsKey (ch) ? charCount[ch]++ : charCount[ch]=1;
+         }
       }
    }
+var sortedCharCount = from entry in charCount orderby entry.Value descending select entry;
+foreach (var kvp in sortedCharCount.Take (7)) { 
+   Console.WriteLine ($"{kvp.Key}: {kvp.Value}");
 }
