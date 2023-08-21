02e1799e0dfc71297dd6ac6fccb9dadf23fd6128
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-21 15:51:34 +0530

    Made changes in T01

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-int decimalNumber = GetIntegerInput ("Enter a decimal number:");
+int decimalNumber = GetIntegerInput ();
 string binary = Convert.ToString (decimalNumber, 2);
 string hexadecimal = decimalNumber.ToString ("X");
 Console.WriteLine ($"Binary value of {decimalNumber}: {binary}");
 Console.WriteLine ($"Hexadecimal value of {decimalNumber}: {hexadecimal}");
-int GetIntegerInput (string prompt) {
+
+int GetIntegerInput () {
+   Console.Write ("\nEnter a decimal number:");
    while (true) {
-      Console.Write (prompt);
       if (int.TryParse (Console.ReadLine (), out int value)) return value;
       Console.WriteLine ("Invalid input. Please enter an integer.");
    }
