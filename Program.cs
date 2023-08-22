111072a443d6b787bbba7f0f2816e25c16c8c1e5
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-22 12:23:00 +0530

    Modified T07

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 System.Console.OutputEncoding = new UnicodeEncoding ();
+
 for (int i = 1; i <= 8; i++) {
    PrintGrid (i);
    Console.WriteLine ();
@@ -11,6 +12,7 @@ for (int i = 1; i <= 8; i++) {
    if (i != 8) Console.Write ('\u251C');
 }
 PrintGrid (9);
+
 void PrintGrid (int row) {
    if (row == 1) Console.Write ('\u250C');
    if (row == 9) Console.Write ('\u2514');
@@ -28,6 +30,7 @@ void PrintGrid (int row) {
       }
    }
 }
+
 void DisplayChessCoins (int i, int j) {
    switch (i) {
       case 1:
@@ -38,7 +41,7 @@ void DisplayChessCoins (int i, int j) {
          else if (j == 5) Console.Write (" " + '\u2654');
          break;
       case 2: Console.Write (" " + '\u2659'); break;
-      case 7: Console.Write (" " + '\u265F'); break;
+      case 7: Console.Write ("" + '\u265F'); break;
       case 8:
          if (j is 1 or 8) Console.Write (" " + '\u265C');
          else if (j is 3 or 6) Console.Write (" " + '\u265D');
