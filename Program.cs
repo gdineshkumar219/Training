e065fe8f424f88e4a5a1902932fab0517a91acbc
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-22 15:06:53 +0530

    Simplified A04

--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
 ﻿string[] wordList = File.ReadAllLines (@"C:\Users\ganesamoorthydi\Downloads\words.txt");
-Dictionary<char, int> charCount = new Dictionary<char, int> ();
-foreach (string word in wordList) {
-   foreach (char ch in word) {
-      if (char.IsLetterOrDigit (ch)) {
-         var c= charCount.ContainsKey (ch) ? charCount[ch]++ : charCount[ch]=1;
-         }
-      }
-   }
-var sortedCharCount = from entry in charCount orderby entry.Value descending select entry;
-foreach (var kvp in sortedCharCount.Take (7)) { 
-   Console.WriteLine ($"{kvp.Key}: {kvp.Value}");
-}
+var charCount = new Dictionary<char, int> ();
+foreach (string word in wordList) foreach (char ch in word) if (char.IsLetterOrDigit (ch)) charCount[ch] = charCount.ContainsKey (ch) ? charCount[ch] + 1 : 1;
+foreach (var kvp in charCount.OrderByDescending (entry => entry.Value).Take (7)) Console.WriteLine ($"{kvp.Key}: {kvp.Value}");
