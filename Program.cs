f9ef9bbc1118ac3c9e9090a9c8f417ef412a0b84
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-22 16:04:42 +0530

    Modified T04

--- Program.cs
+++ Program.cs
@@ -1,40 +1,29 @@
-﻿int n = GetIntegerInput ("Enter the number of integers to find GCD and LCM:: ");
+﻿int n = GetIntegerInput ("Enter the number of integers to find GCD and LCM: ");
 int[] numbers = new int[n];
-for (int i = 0; i < n; i++) {
-   numbers[i] = GetIntegerInput ("Enter number " + (i + 1) + ": ");
-}
+for (int i = 0; i < n; i++) numbers[i] = GetIntegerInput ("Enter number " + (i + 1) + ": ");
 int gcd = CalculateGCD (numbers);
 int lcm = CalculateLCM (numbers);
 Console.WriteLine ("GCD: " + gcd);
 Console.WriteLine ("LCM: " + lcm);
-static int GetIntegerInput (string prompt) {
+
+int GetIntegerInput (string prompt) {
    int value;
    while (true) {
       Console.Write (prompt);
-      if (int.TryParse (Console.ReadLine (), out value) && value>=0) {
-         return value;
-      } else {
-         Console.WriteLine ("Invalid input. Please enter an integer.");
-      }
+      if (int.TryParse (Console.ReadLine (), out value) && value>=0) return value; else Console.WriteLine ("Invalid input. Please enter an integer.");
    }
 }
+
 int CalculateGCD (int[] numbers) {
    int gcd = numbers[0];
-   for (int i = 1; i < numbers.Length; i++) {
-      gcd = CalculatedGCD (gcd, numbers[i]);
-   }
-   return gcd;
+   for (int i = 1; i < numbers.Length; i++) gcd = CalculatedGCD (gcd, numbers[i]);
+      return gcd;
 }
-int CalculatedGCD (int a, int b) {
-   if (b == 0) {
-      return a;
-   }
-   return CalculatedGCD (b, a % b);
-}
+
+int CalculatedGCD (int a, int b) => (b == 0) ? a :CalculatedGCD (b, a % b);
+
 int CalculateLCM (int[] numbers) {
    int lcm = numbers[0];
-   for (int i = 1; i < numbers.Length; i++) {
-      lcm = (lcm * numbers[i]) / CalculatedGCD (lcm, numbers[i]);
-   }
+   for (int i = 1; i < numbers.Length; i++) lcm = (lcm * numbers[i]) / CalculatedGCD (lcm, numbers[i]);
    return lcm;
 }
