4ee91f4ac9d5f462022c80910d739b71a95672a4
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-22 07:55:06 +0530

    Removed curly braces in T05

--- Program.cs
+++ Program.cs
@@ -7,16 +7,13 @@ int GetIntegerInput () {
    Console.Write ("Enter the number to check whether it's prime or not: ");
    while (true) {
       if (int.TryParse (Console.ReadLine (), out int value)) return value;
-      Console.WriteLine ("Invalid input. Please enter an integer.");
+      Console.Write ("Invalid input. Please enter an integer:");
    }
 }
 
 bool IsPrime (int num) {
    if (num <= 1) return false;
-   for (int i = 2; i * i <= num; i++) {
-      if (num % i == 0) {
-         return false;
-      }
-   }
+   for (int i = 2; i * i <= num; i++) 
+      if (num % i == 0) return false;
    return true;
 }
