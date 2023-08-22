377f5e74c57dbad7ba3cf4a44e4fd49c00985295
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-22 12:55:28 +0530

    modified T10

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿int number = GetIntegerInput ("Enter the number to check whether it is palindrome or not:");
+﻿int number = GetIntegerInput ();
 int originalNumber = number;
 int reversedNumber = 0;
 while (number != 0) {
@@ -9,9 +9,10 @@ while (number != 0) {
 Console.WriteLine ($"The reverse of {originalNumber} is {reversedNumber}.");
 var res = originalNumber == reversedNumber ? $"{originalNumber} is a palindrome." : $"{originalNumber} is not a palindrome.";
 Console.WriteLine (res);
-int GetIntegerInput (string prompt) {
+
+int GetIntegerInput () {
    while (true) {
-      Console.Write (prompt);
+      Console.Write ("Enter the number to check whether it is palindrome or not:");
       if (int.TryParse (Console.ReadLine (), out int value) && value > 0) {
          return value;
       }
