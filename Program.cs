2a08fd711289f726de980e9cc7e3d69bc40f9a01
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-22 13:05:33 +0530

    modified T11

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿int number = GetIntegerInput ("Enter the number to find the digital root:");
+﻿int number = GetIntegerInput ();
 Console.WriteLine ($"The digital root value of {number} is {CalculateDigitalRoot (number)}");
-int GetIntegerInput (string prompt) {
+
+int GetIntegerInput () {
    while (true) {
-      Console.Write (prompt);
+      Console.Write ("Enter the number to find the digital root:");
       if (int.TryParse (Console.ReadLine (), out int value) && value > 0) {
          return value;
       }
       Console.WriteLine ("Invalid input. Please enter a positive integer.");
    }
 }
+
 int CalculateDigitalRoot (int n) {
    while (n > 9) {
       int sum = 0;
