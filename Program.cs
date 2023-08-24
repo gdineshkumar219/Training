9cb30dd60c0a8e0b3c4d2bce6c87f608bafbfbe4
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-24 08:50:16 +0530

    Updated the changes in A04

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] wordList = File.ReadAllLines (@"C:\Users\ganesamoorthydi\Downloads\words.txt");
+﻿string[] wordList = File.ReadAllLines ("c:/Training/words.txt");
 var charCount = new Dictionary<char, int> ();
-foreach (string word in wordList) foreach (char ch in word) if (char.IsLetterOrDigit (ch)) charCount[ch] = charCount.ContainsKey (ch) ? charCount[ch] + 1 : 1;
+foreach (string word in wordList) foreach (char ch in word) charCount[ch] = charCount.TryGetValue (ch, out int count) ? charCount[ch] + 1 : 1;
 foreach (var kvp in charCount.OrderByDescending (entry => entry.Value).Take (7)) Console.WriteLine ($"{kvp.Key}: {kvp.Value}");
