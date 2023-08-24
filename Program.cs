b72b9bdb16abf144bd8dab32ff279619b02c78f1
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-24 11:58:49 +0530

    Updated the changes in A04

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] wordList = File.ReadAllLines ("c:/Training/words.txt");
-var charCount = new Dictionary<char, int> ();
-foreach (string word in wordList) foreach (char ch in word) charCount[ch] = charCount.TryGetValue (ch, out int count) ? charCount[ch] + 1 : 1;
+﻿string[] wordList = File.ReadAllLines ("C:\\etc\\words.txt") ;
+Dictionary < char, int>charCount = new Dictionary <char, int> ();
+foreach (string word in wordList) foreach (char ch in word) charCount[ch] = charCount.TryGetValue (ch, out int count) ? count + 1 : 1;
 foreach (var kvp in charCount.OrderByDescending (entry => entry.Value).Take (7)) Console.WriteLine ($"{kvp.Key}: {kvp.Value}");
