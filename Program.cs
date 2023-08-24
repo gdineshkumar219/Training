402045562eaf81a955509d3d00937d7a8b862f23
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-24 17:30:49 +0530

    Updated the changes in T13

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
-﻿using System.Text.RegularExpressions;
+﻿string password = GetPassword ();
 
-Console.Write("Enter the password: ");
-string password = Console.ReadLine ();
-var hasNumber = new Regex (@"[0-9]+");
-var hasUpperChar = new Regex (@"[A-Z]+");
-var hasMinMaxChars = new Regex (@".{6,}");
-var hasLowerChar = new Regex (@"[a-z]+");
-var hasSymbols = new Regex (@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
-var isValidated = hasNumber.IsMatch (password) && hasUpperChar.IsMatch (password) && hasMinMaxChars.IsMatch (password) && hasLowerChar.IsMatch(password)&& hasSymbols.IsMatch(password);
-Console.WriteLine (isValidated);
+string GetPassword () {
+   string password;
+   while (true) {
+      Console.Write ("Enter the password: ");
+      password = Console.ReadLine ();
+      Console.WriteLine (passwordWarnings (password));
+      if (IsValidPassword (password)) {
+         Console.WriteLine ("Password is strong.");
+         break;
+      } else Console.WriteLine ("Invalid password. Please try again.");
+   }
+   return password;
+}
 
+string passwordWarnings (string password) {
+   string warnings = "";
+   warnings += (password.Length < 6) ? "\n!Password must have 6 characters or more." : "\n!Password has more than 5 character.";
+   warnings += (password.Any (char.IsDigit) is false) ? "\n!Password must have atleast 1 number." : "\n!Password has a number.";
+   warnings += (password.Any (char.IsUpper) is false) ? "\n!Password must have atleast one Upper case letter." : "\n!Password has an upper case letter.";
+   warnings += (password.Any (char.IsLower) is false) ? "\n!Password must have atleast one lower case letter." : "\n!Password has a lower case letter.";
+   warnings += (password.Any (ch => !Char.IsLetterOrDigit (ch)) is false) ? "\n!Password must have atleast 1 symbol." : "\n!Password has a symbol.";
+   return warnings;
+}
+
+bool IsValidPassword (string password) => password.Length >= 6 && password.Any (char.IsUpper) && password.Any (char.IsLower) && password.Any (ch => !Char.IsLetterOrDigit (ch)) && password.Any (char.IsDigit);
