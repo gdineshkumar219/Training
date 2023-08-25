5dd934cdf6987743b24afa0668152db1f7d02fb6
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-25 16:16:47 +0530

    Made changes in T08

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿int number = GetIntegerInput ();
-for (int i = 1; i <= 10; i++) Console.Write ($"{number} * {i,2} = {number * i}\n");
-
-int GetIntegerInput () {
-   while (true) {
-      Console.Write ("Enter the multiplication table you want:");
-      if (int.TryParse (Console.ReadLine (), out int value) && value > 0) return value;
-      Console.WriteLine ("Invalid input. Please enter an integer!!!");
+﻿for (int i = 1; i <= 10; i++) {
+   Console.WriteLine ($"Multiplication Table for {i}:");
+   for (int j = 1; j <= 10; j++) {
+      int result = i * j;
+      Console.WriteLine ($"{i,2} * {j,2} = {result}");
    }
+   Console.WriteLine ();
 }
