73bf9f0afd09ddd2891d50df57ceb935402b6720
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-25 08:40:21 +0530

    MOdified T13

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@ string GetPassword () {
       password = Console.ReadLine ();
       Console.WriteLine (passwordWarnings (password));
       if (IsValidPassword (password)) {
-         Console.WriteLine ("Password is strong.");
+         Console.WriteLine (" =>Your Password is strong.");
          break;
-      } else Console.WriteLine ("Invalid password. Please try again.");
+      } else Console.WriteLine ("=>Your password is Weak. Please try again.");
    }
    return password;
 }
 
 string passwordWarnings (string password) {
    string warnings = "";
-   warnings += (password.Length < 6) ? "\n!Password must have 6 characters or more." : "\n!Password has more than 5 character.";
-   warnings += (password.Any (char.IsDigit) is false) ? "\n!Password must have atleast 1 number." : "\n!Password has a number.";
-   warnings += (password.Any (char.IsUpper) is false) ? "\n!Password must have atleast one Upper case letter." : "\n!Password has an upper case letter.";
-   warnings += (password.Any (char.IsLower) is false) ? "\n!Password must have atleast one lower case letter." : "\n!Password has a lower case letter.";
-   warnings += (password.Any (ch => !Char.IsLetterOrDigit (ch)) is false) ? "\n!Password must have atleast 1 symbol." : "\n!Password has a symbol.";
+   warnings += (password.Length < 6) ? "\n!!!Password must have 6 characters or more." : "\nPassword has more than 5 character.";
+   warnings += (password.Any (char.IsDigit) is false) ? "\n!!!Password must have atleast 1 number." : "\nPassword has a number.";
+   warnings += (password.Any (char.IsUpper) is false) ? "\n!!!Password must have atleast one Upper case letter." : "\nPassword has an upper case letter.";
+   warnings += (password.Any (char.IsLower) is false) ? "\n!!!Password must have atleast one lower case letter." : "\nPassword has a lower case letter.";
+   warnings += (password.Any (ch => !Char.IsLetterOrDigit (ch)) is false) ? "\n!Password must have atleast 1 symbol." : "\nPassword has a symbol.";
    return warnings;
 }
 
