24cf00a8986a59fb0a59c637813e2739469d5c5b
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-25 14:33:03 +0530

    started T14

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
-   }
+﻿Console.Write ("Enter the string:");
+string inputString=Console.ReadLine ();
+Console.WriteLine (StringReduction(inputString));
+string StringReduction(string str) {
+   string result = "";
+   char[] chars = str.ToCharArray ();
+   for (int i=0; i < str.Length; i++) 
+      if (chars[i] != chars[i + 1]) { result += chars[i] ; }
+      
+   
+   return result;
 }
