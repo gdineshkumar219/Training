be24da1b369ca72cc21cdc68d385f0b8d629b425
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-25 16:41:09 +0530

    Made changes in T08

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿for (int i = 1; i <= 10; i++) {
    Console.WriteLine ($"Multiplication Table for {i}:");
-   for (int j = 1; j <= 10; j++) {
-      int result = i * j;
-      Console.WriteLine ($"{i,2} * {j,2} = {result}");
-   }
+   for (int j = 1; j <= 10; j++) Console.WriteLine ($"{i,2} * {j,2} = {i*j}");
    Console.WriteLine ();
 }
