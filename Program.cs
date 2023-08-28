e08fe9f597d81e37b8fb7f9b0ce254163ee174a3
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-28 11:13:39 +0530

    Modified the code.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string words = File.ReadAllText ("C:/etc/words.txt") ;
-Dictionary<char, int> charCount = new();
-foreach (char ch in words) if (char.IsLetter(ch))charCount[ch] = charCount[ch] = charCount.GetValueOrDefault (ch) + 1; ;
+﻿string words = File.ReadAllText ("C:/etc/words.txt");
+Dictionary<char, int> charCount = new ();
+foreach (char ch in words) if (char.IsLetter (ch)) charCount[ch] = charCount[ch] = charCount.GetValueOrDefault (ch) + 1; 
 foreach (var kvp in charCount.OrderByDescending (entry => entry.Value).Take (7)) Console.WriteLine ($"{kvp.Key}: {kvp.Value}");
