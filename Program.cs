76442674db12e6d8000db9143244e318162607dd
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-28 08:23:53 +0530

    Updated the changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿Console.Write ("Enter the string:");
 string inputString = Console.ReadLine ();
 Console.WriteLine ($"Is {inputString} ISOGRAM? {IsIsogram (inputString)}");
-static bool IsIsogram (string str) {               
-   return str.Where (char.IsLetter).GroupBy (char.ToLower).All (g => g.Count () == 1);
-}
+static bool IsIsogram (string str) => str.Where (char.IsLetterOrDigit).GroupBy (char.ToLower).All (g => g.Count () == 1);
