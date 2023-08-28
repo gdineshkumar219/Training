7655e07dc60f20cb055b539e995994e11d341139
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-28 14:58:53 +0530

    Updated

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿int n = GetIntegerInput ("Enter the number of integers to find GCD and LCM: ");
 int[] numbers = new int[n];
 for (int i = 0; i < n; i++) numbers[i] = GetIntegerInput ($"Enter number{i + 1}: ");
-int gcd = InputForGCD (numbers);
+int gcd = CalculateGCD (numbers);
 int lcm = CalculateLCM (numbers);
 Console.WriteLine ($"GCD:{gcd}");
 Console.WriteLine ($"LCM: {lcm}");
@@ -14,16 +14,16 @@ int GetIntegerInput (string prompt) {
    }
 }
 
-int InputForGCD (int[] numbers) {
+int CalculateGCD (int[] numbers) {
    int gcd = numbers[0];
-   for (int i = 1; i < numbers.Length; i++) gcd = numbers[i] == 0 ? gcd : CalculateGCD (gcd, numbers[i]);
+   for (int i = 1; i < numbers.Length; i++) gcd = numbers[i] == 0 ? gcd : CalculateGCDforTwoNumbers (gcd, numbers[i]);
    return gcd;
 }
 
-int CalculateGCD (int a, int b) => (b == 0) ? a : CalculateGCD (b, a % b);
+int CalculateGCDforTwoNumbers (int a, int b) => (b == 0) ? a : CalculateGCDforTwoNumbers (b, a % b);
 
 int CalculateLCM (int[] numbers) {
    int lcm = numbers[0];
-   for (int i = 1; i < numbers.Length; i++) lcm = (lcm * numbers[i]) / CalculateGCD (lcm, numbers[i]);
+   for (int i = 1; i < numbers.Length; i++) lcm = (lcm * numbers[i]) / CalculateGCDforTwoNumbers (lcm, numbers[i]);
    return lcm;
 }
