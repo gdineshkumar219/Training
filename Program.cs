0194f436f4e061c92c1e81a5bcb53e51e399bbd4
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-28 07:59:14 +0530

    Updated the changes

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@ for (i = 0; i < rows; i++) {
    for (blank = 1; blank <= rows - i; blank++) Console.Write ("  ");
    for (j = 0; j <= i; j++) {
       val = j == 0 || i == 0 ? 1 : val * (i - j + 1) / j;
-      Console.Write ($"{val,6}");
+      Console.Write ($"{val,5}");
    }
    Console.WriteLine ();
 }
 
 int GetIntegerInput () {
    while (true) {
-      Console.Write ("Enter the number of rows:");
+      Console.Write ("Enter the number of rows: ");
       if (int.TryParse (Console.ReadLine (), out int value) && value > 0) return value;
 
       Console.WriteLine ("Invalid input. Please enter a positive integer.");
