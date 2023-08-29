12af82b74ead266a30e8d475048f51a7ecdd8aac
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-29 17:04:11 +0530

    T19 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿int input = GetIntegerInput ();
+Console.WriteLine (CalculateFactorial(input));
+
+int GetIntegerInput () {
+   while (true) {
+      Console.Write ("\nEnter a number to check whether it is ARMSTRONG number or not: ");
+      if (int.TryParse (Console.ReadLine (), out int value)&& value>0) return value;
+      Console.WriteLine ("Invalid input. Please enter an integer.");
    }
 }
+
+int CalculateFactorial(int n) =>Enumerable.Range (1, n).Aggregate (1, (p, item) => p * item);
