c7eae756caa2e65cefd49107c127be22a41f9dd5
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-29 11:35:41 +0530

    T16 completed.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿string[] wordArray = { "a", "ab", "abc", "abcdef", "adtfiwft" };
+Console.WriteLine (ToFindLongestAbecedarian (wordArray));
+
+static string ToFindLongestAbecedarian (string[] wordArray) {
+   if (!wordArray.Any ()) return "Empty array!";
+   string result = "";
+   foreach (string word in wordArray) {
+      char[] chars = word.ToCharArray ();
+      Array.Sort (chars);
+      string sortedWord = new (chars);
+      if (sortedWord == word && word.Length > result.Length) result = word;
    }
+   return result;
 }
