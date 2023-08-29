8e8c83d7e5d2759b333b5e6ff63f28c3c3cd596c
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-29 14:13:32 +0530

    T17 completed.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
-   }
-}
+﻿Console.Write ("Enter the string: ");
+string input = Console.ReadLine ();
+string[] permutations = GetPermutations (input);
+Console.WriteLine ($"The combination of {input}");
+for (int i = 0; i < permutations.Length; i++) Console.WriteLine ($"{i + 1,3}.{permutations[i]}");
+
+static string[] GetPermutations (string input) => (input.Length == 1) ? new string[] { input } : input.SelectMany ((c, i) => GetPermutations (input.Remove (i, 1)).Select (perm => c + perm)).ToArray ();
