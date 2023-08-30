e2cfbec33d61d04217daa106456d311ca5cd884e
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-30 09:01:49 +0530

    T21 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿int a = GetIntegerInput ();
+int b = GetIntegerInput ();
+Console.WriteLine ("The values before swapping:");
+Console.WriteLine ($"a={a}");
+Console.WriteLine ($"b={b}");
+SwapTwoValues (a, b);
+
+int GetIntegerInput () {
+   while (true) {
+      Console.Write ("Enter a numbers to Swap: ");
+      if (int.TryParse (Console.ReadLine (), out int value) && value > 0) return value;
+      Console.WriteLine ("Invalid input. Please enter an integer.");
    }
+}
+void SwapTwoValues (int a, int b) {
+   (a, b) = (b, a);
+   Console.WriteLine ("The values after swapping: ");
+   Console.WriteLine ($"a={a}");
+   Console.WriteLine ($"b={b}");
 }
