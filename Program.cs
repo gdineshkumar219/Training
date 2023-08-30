9844c81bbe8fc3662e4d52e149a56e5c1b91f8fc
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-30 12:20:48 +0530

    Removed var msg

--- Program.cs
+++ Program.cs
@@ -1,18 +1,16 @@
 ﻿int inputNumber = GetIntegerInput ();
-var msg = IsArmstrongNumber (inputNumber) is true ? $"{inputNumber} is an ARMSTRONG number." : $"{inputNumber} is not an ARMSTRONG number.";
-Console.WriteLine (msg);
+Console.WriteLine (IsArmstrongNumber (inputNumber) is true ? $"{inputNumber} is an ARMSTRONG number." : $"{inputNumber} is not an ARMSTRONG number.");
 
 int GetIntegerInput () {
    while (true) {
       Console.Write ("\nEnter a number to check whether it is ARMSTRONG number or not: ");
-      if (int.TryParse (Console.ReadLine (), out int value)) return value;
+      if (int.TryParse (Console.ReadLine (), out int value) && value > 0) return value;
       Console.WriteLine ("Invalid input. Please enter an integer.");
    }
 }
 
 bool IsArmstrongNumber (int num) {
-   string numStr = num.ToString ();
-   int p = numStr.Length;
+   int p = num.ToString ().Length;
    int result = 0;
    for (int i = num; i > 0; i /= 10) result += (int)Math.Pow (i % 10, p);
    return result == num;
