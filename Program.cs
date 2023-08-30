785b4841ee45ada71b71e45fdc3000ecd72c164e
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-30 08:19:04 +0530

    Modified T19

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿int input = GetIntegerInput ();
-Console.WriteLine (CalculateFactorial(input));
+Console.WriteLine ($"The factorial of {input}! is {CalculateFactorial (input)}");
 
 int GetIntegerInput () {
    while (true) {
-      Console.Write ("\nEnter a number to check whether it is ARMSTRONG number or not: ");
-      if (int.TryParse (Console.ReadLine (), out int value)&& value>0) return value;
+      Console.Write ("Enter a number to find the factorial : ");
+      if (int.TryParse (Console.ReadLine (), out int value) && value > 0) return value;
       Console.WriteLine ("Invalid input. Please enter an integer.");
    }
 }
 
-int CalculateFactorial(int n) =>Enumerable.Range (1, n).Aggregate (1, (p, item) => p * item);
+int CalculateFactorial (int n) => Enumerable.Range (1, n).Aggregate (1, (p, item) => p * item);
