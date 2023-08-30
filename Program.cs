045f9ee454c93498b8b452a583508ebc762b8d37
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-30 08:46:10 +0530

    T20 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿decimal inputNumber = GetInput ();
+DisplayParts (inputNumber);
+
+decimal GetInput () {
+   while (true) {
+      Console.Write ("Enter a decimal number: ");
+      if (decimal.TryParse (Console.ReadLine (), out decimal value) && value > 0) return value;
+      Console.WriteLine ("Invalid input. Please enter a valid decimal number");
    }
+}
+void DisplayParts (decimal number) {
+   decimal integralPart = Math.Floor (number);
+   decimal fractionalPart = number - integralPart;
+   Console.WriteLine ($"Integral part: {GetDigits (integralPart)}");
+   Console.WriteLine ($"Fractional part: {GetDigits (fractionalPart)}");
+}
+
+string GetDigits (decimal number) {
+   string digits = "";
+   string numberString = number.ToString ("");
+   foreach (char digitChar in numberString) if (char.IsDigit (digitChar) || digitChar == '.') digits += digitChar + " ";
+   return digits.Trim ();
 }
