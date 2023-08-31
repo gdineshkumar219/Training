04a37f0b0768d116acaf4147281ba016dd4933e9
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-31 14:37:00 +0530

    T22 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿int n = GetIntegerInput ("Enter the number of integers u want: ");
+int[] numbers = new int[n];
+for (int i = 0; i < n; i++) numbers[i] = GetIntegerInput ($"Enter number {i + 1}: ");
+Console.WriteLine ("\nArray before swapping: ");
+foreach (var c in numbers) { Console.Write ($"{c}\t"); }
+Console.WriteLine ($"\nEnter the index to be swapped.\nEnter the index value between 0 to {n - 1}. ");
+int pos1 = GetIndexInput ("Enter the value of index1: ");
+int pos2 = GetIndexInput ("Enter the value of index2: ");
+
+int GetIntegerInput (string prompt) {
+   while (true) {
+      Console.Write ($"{prompt}");
+      if (int.TryParse (Console.ReadLine (), out int value) && value >= 0) return value;
+      else Console.WriteLine ("Invalid input. Please enter an integer.");
    }
 }
+
+int GetIndexInput (string prompt) {
+   while (true) {
+      Console.Write ($"{prompt}");
+      if (int.TryParse (Console.ReadLine (), out int value) && value < n) return value;
+      else Console.WriteLine ($"Invalid input. Please enter an integer value between 0 and {n - 1}.");
+   }
+}
+static int[] ToSwap (int[] arr, int a, int b) {
+   (arr[a], arr[b]) = (arr[b], arr[a]);
+   return arr;
+}
+Console.WriteLine ("\nArray after swapping: ");
+foreach (var ch in ToSwap (numbers, pos1, pos2)) { Console.Write ($"{ch}\t"); }
