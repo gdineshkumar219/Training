2ae04f62718b87d8d24a6984dcd38efddedf1a30
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-31 14:45:43 +0530

    T22 completed

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ foreach (var c in numbers) Console.Write ($"{c}\t");
 Console.WriteLine ($"\nEnter the index to be swapped.\nEnter the index value between 0 to {n - 1}. ");
 int pos1 = GetIndexInput ("Enter the value of index1: ");
 int pos2 = GetIndexInput ("Enter the value of index2: ");
+Console.WriteLine ("\nArray after swapping: ");
+foreach (var ch in ToSwap (numbers, pos1, pos2)) Console.Write ($"{ch}\t");
 
 int GetIntegerInput (string prompt) {
    while (true) {
@@ -27,5 +29,3 @@ static int[] ToSwap (int[] arr, int a, int b) {
    (arr[a], arr[b]) = (arr[b], arr[a]);
    return arr;
 }
-Console.WriteLine ("\nArray after swapping: ");
-foreach (var ch in ToSwap (numbers, pos1, pos2)) Console.Write ($"{ch}\t");
