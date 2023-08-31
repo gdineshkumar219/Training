012608f553a6f2f0b43aa6303bb272dcbcab0712
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-31 12:27:30 +0530

    Modified using if-else statements

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ Console.WriteLine ($"The reduced string is {StringReduction (inputString)}");
 
 static string StringReduction (string str) {
    string result = "";
-   foreach (char ch in str) result = (result.Length > 0 && result[result.Length - 1] == ch) ? result.Substring (0, result.Length - 1) : result + ch;
+   for (int i = 0; i < str.Length; i++) {
+      if (i == str.Length - 1 || str[i] != str[i + 1]) result += str[i];
+      else i++;
+   }
    return result;
 }
