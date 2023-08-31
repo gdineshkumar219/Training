e5e16a74c80266941ddcc1645661d09fb78c9dd2
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-08-31 16:28:39 +0530

    Added comments explaining GetPermutations

--- Program.cs
+++ Program.cs
@@ -3,5 +3,10 @@ string input = Console.ReadLine ();
 string[] permutations = GetPermutations (input);
 Console.WriteLine ($"The combination of {input}");
 for (int i = 0; i < permutations.Length; i++) Console.WriteLine ($"{i + 1,3}.{permutations[i]}");
+//BaseCase: If the input has only one character ,return an array with that character.
+//RecursiveCase:Iterate through each character of input string-->input.SelectMany (c, i)
+//Create a new string without the current character-->input.Remove (i, 1)
+//Recursively call on remaining characters creates substrings
+//Combine the current character with each substring and return the concatenated array-->.Select (perm => c + perm)
 
 static string[] GetPermutations (string input) => (input.Length == 1) ? new string[] { input } : input.SelectMany ((c, i) => GetPermutations (input.Remove (i, 1)).Select (perm => c + perm)).ToArray ();
