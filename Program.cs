41405cf8396b59339e2d7a74bc08fcf38f1715ce
gdineshkumar219 <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-09-27 12:33:42 +0530

    Modified the logic to get the mentioned result

--- Program.cs
+++ Program.cs
@@ -5,13 +5,11 @@
       static void Main () {
          string input = GetInput ("Enter the input characters: ");
          char specialChar = GetSpecialCharacter ();
-         string sortOrder = GetSortOrder ();
-         string result = SortAndAddSpecialCharToEnd (input.ToCharArray (), specialChar, sortOrder);
+         bool isDescending = IsDescending ();
+         string result = SortAndAddSpecialCharToEnd (input.ToCharArray (), specialChar, isDescending);
          Console.WriteLine ($"Sorted and moved special character: {result}");
 
-         /// <summary>
-         /// Prompts the user for input and validates that it contains only letters.
-         /// </summary>
+         /// <summary>Prompts the user for input and validates that it contains only letters.</summary>
          /// <param name="prompt">The prompt message.</param>
          /// <returns>The user-provided input containing only letters.</returns>
          static string GetInput (string prompt) {
@@ -20,22 +18,21 @@
                Console.Write ($"{prompt}");
                input = Console.ReadLine ();
                if (!input.Any (char.IsDigit) && input.Length > 0) return input;
-               Console.WriteLine ("Input should contain only letters . Please try again.");
+               Console.WriteLine ("Input should contain only letters. Please try again.");
             }
          }
 
          /// <summary>Prompts the user for the desired sort order and returns it.</summary>
          /// <returns>The sort order ("ascending" or "descending").</returns>
-         static string GetSortOrder () {
+         static bool IsDescending () {
             Console.WriteLine ("Do you want to sort the array in descending order? (y)es or (n)o");
-            char orderKey = Console.ReadKey (true).KeyChar;
-            return orderKey == 'y' ? "descending" : "ascending";
+            return Console.ReadKey (true).KeyChar == 'y';
          }
 
          /// <summary>Prompts the user for a special character.</summary>
          /// <returns>The special character entered by the user.</returns>
          static char GetSpecialCharacter () {
-            Console.Write ("Enter the special character you added.Otherwise,enter any character: ");
+            Console.Write ("Enter the special character you added. Otherwise, enter any character: ");
             char specialChar = Console.ReadKey ().KeyChar;
             Console.WriteLine ();
             return specialChar;
@@ -46,10 +43,10 @@
          /// <param name="specialChar">The special character to be filtered out and appended.</param>
          /// <param name="sortOrder">The desired sort order ("ascending" or "descending"). Default is "ascending".</param>
          /// <returns>The sorted characters with the special character appended.</returns>
-         static string SortAndAddSpecialCharToEnd (char[] chArr, char splCh, string sortOrder = "ascending") {
-            char[] filteredArr = chArr.Where (c => char.IsLetter (c)).ToArray ();
-            char[] splChArr = chArr.Where (c => c == splCh && !char.IsLetter (c)).ToArray ();
-            var sortedArr = sortOrder == "ascending" ? filteredArr.Order () : filteredArr.OrderDescending ();
+         static string SortAndAddSpecialCharToEnd (char[] arr, char spl, bool isDescending = false) {
+            char[] filteredArr = arr.Where (c => c != spl).ToArray ();
+            char[] splChArr = arr.Where (c => c == spl).ToArray ();
+            var sortedArr = isDescending == false ? filteredArr.Order () : filteredArr.OrderDescending ();
             return string.Join (", ", sortedArr.Concat (splChArr));
          }
       }
