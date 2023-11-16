9f8adbc27c26dc35ae1f5fc8d0db256bfb8db1ce
Dinesh kumar <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-11-16 13:37:25 +0530

    Added test folder

---
+++ ClassLibrary/Classlib.cs
@@ -0,0 +1,11 @@
+﻿// --------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2023.
+// Copyright (c) Metamation India.                                              
+// ------------------------------------------------------------------------
+// Program.cs
+// -------------------------------------------------------------------------------------
+namespace ClassLibrary {
+public class Classlib {
+
+   }
+}
---
+++ Training/Program.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2023.
+// Copyright (c) Metamation India.                                              
+// ------------------------------------------------------------------------
+// Program.cs
+// Simple Program to Print the text 'Hello, World!' in Console
+// --------------------------------------------------------------------------------------------
+
+namespace Training {
+   internal class Program {
+      static void Main (string[] args) {
+         Console.WriteLine ("Hello, World!");
+      }
+   }
+}
