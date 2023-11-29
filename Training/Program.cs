9c9481bcc21cd3988ae839dbe45ca78b8fbafe7b
Dinesh kumar <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-11-29 17:33:26 +0530

    Started expresion evaluator

--- Training/Program.cs
+++ Training/Program.cs
@@ -6,10 +6,24 @@
 // Simple Program to Print the text 'Hello, World!' in Console
 // --------------------------------------------------------------------------------------------
 
-namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+namespace Training;
+
+class Program {
+   static void Main (string[] args) {
+      var eval = new Evaluator ();
+      for (; ; ) {
+         Console.Write ("> ");
+         string text = Console.ReadLine () ?? "";
+         if (text == "exit") break;
+         try {
+            double result = eval.Evaluate (text);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine (result);
+         } catch (Exception e) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine (e.Message);
+         }
+         Console.ResetColor ();
       }
    }
 }
---
+++ Training/Tokenizer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Training;
+
+namespace Training;
+
+class Tokenizer {
+   public Tokenizer (Evaluator eval, string text) {
+      mText = text; mN = 0; mEval = eval;
+   }
+   readonly Evaluator mEval;  // The evaluator that owns this 
+   readonly string mText;     // The input text we're parsing through
+   int mN;                    // Position within the text
+
+   public Token Next (List<Token> tokens) {
+      while (mN < mText.Length) {
+         char ch = char.ToLower (mText[mN++]);
+         switch (ch) {
+            case ' ' or '\t': continue;
+            case (>= '0' and <= '9') or '.': return GetNumber ();
+            case '(' or ')': return new TPunctuation (ch);
+            case '+' or '-':
+               if (tokens.Count == 0 || tokens[^1] is TOperator { Priority: _ } || (tokens[^1] is TPunctuation { Punct: '(' }))
+                  return new TOpUnary (mEval, ch);
+               return new TOpArithmetic (mEval, ch);
+            case '*' or '/': return new TOpArithmetic (mEval, ch);
+            case '=': return new TOpArithmetic (mEval, ch);
+            case '^': return new TOpArithmetic (mEval, ch);
+            case >= 'a' and <= 'z': return GetIdentifier ();
+            default: return new TError ($"Unknown symbol: {ch}");
+         }
+      }
+      return new TEnd ();
+   }
+
+   Token GetIdentifier () {
+      int start = mN - 1;
+      while (mN < mText.Length) {
+         char ch = char.ToLower (mText[mN++]);
+         if (ch is >= 'a' and <= 'z') continue;
+         mN--; break;
+      }
+      string sub = mText[start..mN];
+      if (mFuncs.Contains (sub)) return new TOpFunction (mEval, sub);
+      else return new TVariable (mEval, sub);
+   }
+   readonly string[] mFuncs = { "sin", "cos", "tan", "sqrt", "log", "exp", "asin", "acos", "atan" };
+
+   Token GetNumber () {
+      int start = mN - 1;
+      while (mN < mText.Length) {
+         char ch = mText[mN++];
+         if (ch is (>= '0' and <= '9') or '.') continue;
+         mN--; break;
+      }
+      // Now, mN points to the first character of mText that is not part of the number
+      string sub = mText[start..mN];
+      if (double.TryParse (sub, out double f)) return new TLiteral (f);
+      return new TError ($"Invalid number: {sub}");
+   }
+}
