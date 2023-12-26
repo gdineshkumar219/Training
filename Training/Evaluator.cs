7ecd535393e3eb2598d36eb3a140cc4188321c95
Dinesh kumar <Dineshkumar.Ganesamoorthy@trumpf.com>
2023-12-26 15:53:16 +0530

    commit1

--- Training/Evaluator.cs
+++ Training/Evaluator.cs
@@ -6,32 +6,22 @@
 // Program to solve mathematical expressions.
 // ------------------------------------------------------------------------------------------------
 namespace Training;
-
 public class EvalException : Exception {
    public EvalException (string message) : base (message) { }
 }
-#region Class Evaluator ---------------------------------------------
-/// <summary>
-/// Represents an expression evaluator that can process mathematical expressions
-/// </summary>
 public class Evaluator {
-   #region Methods --------------------------------------------------
-   /// <summary>Evaluates the specified mathematical expression and returns the result</summary>
-   /// <param name="text">The mathematical expression to be evaluated</param>
-   /// <returns>The result of the evaluation</returns>
    public double Evaluate (string text) {
       Reset ();
-      // Create a tokenizer for the expression
       List<Token> tokens = new ();
       var tokenizer = new Tokenizer (this, text);
-      // Tokenize the expression and add tokens to the list
       for (; ; ) {
          var token = tokenizer.Next (tokens);
          if (token is TEnd) break;
          if (token is TError err) throw new EvalException (err.Message);
          tokens.Add (token);
       }
-      // Check for variable assignment at the beginning of the expression
+
+      // Check if this is a variable assignment
       TVariable tVariable = null!;
       if (tokens.Count > 1 && tokens[0] is TVariable tv && tokens[1] is TOpArithmetic { Op: '=' }) {
          tVariable = tv;
@@ -43,73 +33,53 @@ public class Evaluator {
             tokens.Remove (un);
          }
       }
-      // Process each token in the expression
       foreach (Token t in tokens) Process (t);
       while (mOperators.Count > 0) ApplyOperator ();
       if (mOperators.Count != 0) throw new EvalException ("Excessive use of operators");
       if (mOperands.Count != 1) throw new EvalException ("Excessive use of operands");
       if (BasePriority != 0) throw new EvalException ("Mismatched Paranthesis");
-      // Retrieve the final result and round it to 10 decimal places
       double f = Math.Round (mOperands.Pop (), 10);
-      // If a variable was assigned, store the result in the variables dictionary
       if (tVariable != null) mVars[tVariable.Name] = f;
       return f;
    }
 
-   /// <summary>Retrieves the value of the specified variable from the evaluator's variables</summary>
-   /// <param name="name">The name of the variable to retrieve</param>
-   /// <returns>The value of the specified variable</returns>
+   public int BasePriority { get; set; }
+
    public double GetVariable (string name) {
-      // Try to retrieve the variable value from the dictionary
       if (mVars.TryGetValue (name, out double f)) return f;
-      // If the variable is not found, throw an EvalException
       throw new EvalException ($"Unknown variable: {name}");
    }
-   #endregion
+   readonly Dictionary<string, double> mVars = new ();
 
-   #region Implementation -------------------------------------------
-   /// <summary> Processes the given token during expression evaluation</summary>
-   /// <param name="token">The token to be processed</param>
    void Process (Token token) {
-      // Switch on the type of the token
       switch (token) {
          case TNumber num:
-            // If the token is a number, push its value to the operand stack
             mOperands.Push (num.Value);
             break;
          case TOperator op:
-            // If the token is an operator, handle its processing
             if (mOperators.Count > 0 && mOperators.Peek ().Priority > op.Priority)
                ApplyOperator ();
             mOperators.Push (op);
             break;
          case TPunctuation p:
-            // If the token is punctuation, check for '(' and apply operators
-            //if (p.Punct == '(') break;
-            //ApplyOperator ();
-            //break;
             BasePriority += p.Punct == '(' ? 10 : -10;
             break;
          default:
-            // If the token type is unknown, throw an EvalException
             throw new EvalException ($"Unknown token: {token}");
       }
    }
+   readonly Stack<double> mOperands = new ();
+   readonly Stack<TOperator> mOperators = new ();
 
-   /// <summary> Applies the top operator from the stack during expression evaluation</summary>
    void ApplyOperator () {
-      // Pop the top operator from the stack
       TOperator op = mOperators.Pop ();
       double a;
       try {
-         // Try to pop the top operand from the stack
          a = mOperands.Pop ();
       } catch (Exception) {
-         // If an exception occurs, push the operator back and return
          mOperators.Push (op);
          return;
       }
-      // Switch on the type of the operator and apply it to the operands
       switch (op) {
          case TOpFunction fun:
             mOperands.Push (fun.Evaluate (a));
@@ -124,24 +94,10 @@ public class Evaluator {
             break;
       }
    }
-   /// <summary>Clear operand and operator stacks before processing a new expression </summary>
+
    void Reset () {
       mOperands.Clear ();
       mOperators.Clear ();
       BasePriority = 0;
    }
-   #endregion
-
-   /// <summary> Represents the base priority for operators in the evaluator</summary>
-   internal int BasePriority { get; set; }
-
-   #region Private Members ------------------------------------------
-   // Dictionary to store variables and their values
-   readonly Dictionary<string, double> mVars = new ();
-   // Operand and operator stacks used during evaluation
-   readonly Stack<double> mOperands = new ();
-   readonly Stack<TOperator> mOperators = new ();
-   #endregion
 }
-#endregion
-
--- Training/Tokenizer.cs
+++ Training/Tokenizer.cs
@@ -7,58 +7,37 @@
 // of tokens that can be used for evaluation by the associated Evaluator. 
 // ------------------------------------------------------------------------------------------------
 namespace Training;
-#region Class Tokenizer ---------------------------------------------------------------------------
 class Tokenizer {
-   #region Constructor ----------------------------------------------
-   /// <summary>Initializes a new instance of the Tokenizer class</summary>
    public Tokenizer (Evaluator eval, string text) {
       mText = text; mN = 0; mEval = eval;
    }
-   #endregion
+   readonly Evaluator mEval;  // The evaluator that owns this
+   readonly string mText;     // The input text we're parsing through
+   int mN;                    // Position within the text
 
-   #region Methods --------------------------------------------------
-   /// <summary>Retrieves the next token from the input text</summary>
-   /// <param name="tokens">List of tokens being constructed</param>
-   /// <returns>Next token in the input text</returns>
    public Token Next (List<Token> tokens) {
       while (mN < mText.Length) {
-         // Read the next character from the input text
          char ch = mText[mN++];
          switch (ch) {
-            case ' ': continue;  // Skip whitespace
+            case ' ' or '\t': continue;  // Skip whitespace
             case '+' or '-':
-               // Check if the current character is a unary operator or an arithmetic operator
                return (tokens.Count == 0 || tokens[^1] is TOperator or TPunctuation { Punct: '(' })
                    ? new TOpUnary (mEval, ch) : new TOpArithmetic (mEval, ch);
             case '/' or '*' or '^' or '=':
-               // Create an arithmetic operator token
                return new TOpArithmetic (mEval, ch);
             case >= '0' and <= '9':
-               // Create a literal token
                return GetNumber ();
             case >= 'a' and <= 'z':
-               // Create an identifier token
                return GetIdentifier ();
             case '(' or ')':
-               // Adjust evaluator priority based on parentheses
                mEval.BasePriority += ch == '(' ? 10 : -10;
-               // Create a punctuation token
                return new TPunctuation (ch);
             default:
-               // Create an error token for unknown symbols
                return new TError ($"Unknown symbol: {ch}");
          }
       }
-      // Return end of input token
       return new TEnd ();
    }
-   #endregion
-
-   #region Implementation -------------------------------------------
-   /// <summary>
-   /// Gets the identifier token from the current position in the input text
-   /// </summary>
-   /// <returns>Identifier token</returns>
 
    Token GetIdentifier () {
       int start = mN - 1;
@@ -73,8 +52,6 @@ class Tokenizer {
    }
    readonly string[] mFuncs = { "sin", "cos", "tan", "sqrt", "log", "exp", "asin", "acos", "atan" };
 
-   /// <summary>Gets the literal token from the current position in the input text</summary>
-   /// <returns>The literal token</returns>
    Token GetNumber () {
       int start = mN - 1;
       while (mN < mText.Length) {
@@ -87,12 +64,4 @@ class Tokenizer {
       if (double.TryParse (sub, out double f)) return new TLiteral (f);
       return new TError ($"Invalid number: {sub}");
    }
-   #endregion
-
-   #region Private Members ------------------------------------------
-   readonly Evaluator mEval;  // The evaluator that owns this
-   readonly string mText;     // The input text we're parsing through
-   int mN;                    // Position within the text
-   #endregion
 }
-#endregion
