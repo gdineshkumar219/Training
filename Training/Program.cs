6977a21e54d4456ac4047ceaede6f99a9a0d83e9
Dinesh kumar <Dineshkumar.Ganesamoorthy@trumpf.com>
2024-01-24 11:41:35 +0530

    Implemented complex number

--- Training/Program.cs
+++ Training/Program.cs
@@ -6,10 +6,28 @@
 // Simple Program to Print the text 'Hello, World!' in Console
 // --------------------------------------------------------------------------------------------
 
+using System.Numerics;
+
 namespace Training {
    internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+      static void Main () {
+         var x = ComplexNumber.GetComplexNum ();
+         var y = ComplexNumber.GetComplexNum ();
+         var add= ComplexNumber.Addition (x, y);
+         var sub = ComplexNumber.Sub (x, y);
+         var norm = ComplexNumber.Norm (x);
+         Console.Write ($"Addition:");
+         ComplexNumber.Print (add);
+         Console.Write ($"Subtraction:");
+         ComplexNumber.Print (sub);
+         Console.WriteLine ($"Norm of x: {norm}");
+
+
+
+
+
+
+
       }
    }
 }
